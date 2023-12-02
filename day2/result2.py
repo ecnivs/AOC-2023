f63f1a695ddc2225487db0b2a34373b1333dda16
Vince <vinceswu88@gmail.com>
2023-12-03 00:45:42 +0530

    commit

---
+++ day2/result2.py
@@ -0,0 +1,29 @@
+# Aoc Day-2 Cube Conundrum
+# Solution-2
+
+with open("input.txt") as fin:
+    data = fin.read().strip().split("\n")
+
+result = 0
+
+for line in data:
+    game, parts = line.split(": ")
+    r, g, b = 0, 0, 0
+
+    passed = True
+    for part in parts.split("; "):
+        for cubes in part.split(", "):
+            num, color = cubes.split(" ")
+            num = int(num)
+
+            if color == "red":
+                r = max(r, num)
+            if color == "blue":
+                b = max(b, num)
+            if color == "green":
+                g = max(g, num)
+
+    if passed:
+        result += r * b * g
+
+print(result)
